26eb466cd2ffd5e2aeab4ae1c750aca71c61a6df
Hari Om Chadha <chadhahariom1@gmail.com>
2025-03-12 11:07:40 -0400

    Not working version of BME680

--- src/lynktree.cpp
+++ src/lynktree.cpp
@@ -5,6 +5,7 @@ LynkTree::LynkTree() {
     Serial.println("Connecting to Wi-Fi");
     WifiSetup();
     MqttSetup();
+    BmeSetup();
 }
 
 void LynkTree::WifiSetup() {
@@ -63,9 +64,37 @@ void LynkTree::MqttSetup()
     Serial.println("MQTT Connected!");
 }
 
+void LynkTree::BmeSetup()
+{
+    Serial.println(F("BME680 test"));
+  
+    if (!bme_.begin()) {
+        Serial.println("Could not find a valid BME680 sensor, check wiring!");
+        cyw43_arch_gpio_put(CYW43_WL_GPIO_LED_PIN, 1);
+        sleep_ms(250);
+        cyw43_arch_gpio_put(CYW43_WL_GPIO_LED_PIN, 0);
+        sleep_ms(250);
+        cyw43_arch_gpio_put(CYW43_WL_GPIO_LED_PIN, 1);
+        sleep_ms(250);
+        cyw43_arch_gpio_put(CYW43_WL_GPIO_LED_PIN, 0);
+        sleep_ms(250);
+        while (1);
+    }
+  
+    // Set up oversampling and filter initialization
+    bme_.setTemperatureOversampling(BME680_OS_8X);
+    bme_.setHumidityOversampling(BME680_OS_2X);
+    bme_.setPressureOversampling(BME680_OS_4X);
+    bme_.setIIRFilterSize(BME680_FILTER_SIZE_3);
+    bme_.setGasHeater(320, 150); // 320*C for 150 ms
+}
+
+
 void LynkTree::loop()
 {
-    data.publish(x_++);
+    // data.publish(x_++);
+    data.publish(bme_.temperature);
+
 
     // add debug led
     cyw43_arch_gpio_put(CYW43_WL_GPIO_LED_PIN, 1);
