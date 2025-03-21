4115e5530cfc60272cc1a8fa2ed21c1c48420dfb
Hari Om Chadha <chadhahariom1@gmail.com>
2025-03-21 11:20:03 -0400

    changed the  code for checking the battery level. Not tested

--- src/lynktree.cpp
+++ src/lynktree.cpp
@@ -2,12 +2,31 @@
 
 LynkTree::LynkTree() {
     Serial.begin(115200);
+    BatterySetup();
     WifiSetup();
     MqttSetup();
     BmeSetup();
     AccelSetup();
 }
 
+void LynkTree::BatterySetup()
+{
+    if (!cyw43_arch_gpio_get(CYW43_WL_GPIO_VBUS_PIN)) {
+        Debug("Battery powered");
+        using_battery_ = true;
+    } else {
+        Debug("Powered by USB");
+        using_battery_ = false;
+    }
+
+}
+
+void LynkTree::RGBSetup() {
+    // Set up the RGB LED
+    pinMode(red_led, OUTPUT);
+    pinMode(green_led, OUTPUT);
+    pinMode(blue_led, OUTPUT);
+}
 
 void LynkTree::WifiSetup() {
     if (cyw43_arch_init()) {
@@ -52,16 +71,16 @@ void LynkTree::MqttSetup()
         // }
 
     }
-    // Debug("MQTT Connected!");
+    Debug("MQTT Connected!");
 }
 
 void LynkTree::BmeSetup()
 {
     if (!bme_.init()) {
-        // Debug("BME680 sensor not found!");
+        Debug("BME680 sensor not found!");
         return;
     }
-    // Debug("BME680 sensor found!");
+    Debug("BME680 sensor found!");
 
 }
 
@@ -70,17 +89,17 @@ void LynkTree::AccelSetup()
     Wire.begin();
 
     if (!kxAccel_.begin()) {
-        // Debug("KX132 sensor not found!");
+        Debug("KX132 sensor not found!");
         return;
     }
-    // Debug("KX132 sensor found!");
+    //Debug("KX132 sensor found!");
 
     sleep_ms(500);
 
     if (kxAccel_.softwareReset()){
         // Debug("KX132 software reset successful!");
     } else {
-        // Debug("KX132 software reset failed!");
+         Debug("KX132 software reset failed!");
     }
     sleep_ms(5);
 
@@ -101,86 +120,21 @@ void LynkTree::AccelSetup()
     kxAccel_.clearInterrupt();
 
     // kxAccel_.forceSleep();
-
-}
-
-int LynkTree::power_source(bool *battery_powered)
-{
-    #if defined CYW43_WL_GPIO_VBUS_PIN
-        *battery_powered = !cyw43_arch_gpio_get(CYW43_WL_GPIO_VBUS_PIN);
-        Debug("Power source pin defined");
-        return PICO_OK;
-    #endif
-    Debug("No power source pin defined");
-}
-
-int LynkTree::power_voltage(float *voltage_result){
-    #if CYW43_USES_VSYS_PIN
-        cyw43_thread_enter();
-        // Make sure cyw43 is awake
-        cyw43_arch_gpio_get(CYW43_WL_GPIO_VBUS_PIN);
-    #endif
-
-    // setup adc
-    adc_gpio_init(PICO_VSYS_PIN);
-    adc_select_input(PICO_VSYS_PIN - PICO_FIRST_ADC_PIN);
-
-    adc_fifo_setup(true, false, 0, false, false);
-    adc_run(true);
-
-    // We seem to read low values initially - this seems to fix it
-    int ignore_count = PICO_POWER_SAMPLE_COUNT;
-    while (!adc_fifo_is_empty() || ignore_count-- > 0) {
-        (void)adc_fifo_get_blocking();
-    }
-
-    // read vsys
-    uint32_t vsys = 0;
-    for(int i = 0; i < PICO_POWER_SAMPLE_COUNT; i++) {
-        uint16_t val = adc_fifo_get_blocking();
-        vsys += val;
-    }
-
-    adc_run(false);
-    adc_fifo_drain();
-
-    vsys /= PICO_POWER_SAMPLE_COUNT;
-    #if CYW43_USES_VSYS_PIN
-        cyw43_thread_exit();
-    #endif
-        // Generate voltage
-        const float conversion_factor = 3.3f / (1 << 12);
-        *voltage_result = vsys * 3 * conversion_factor;
-        return PICO_OK;
+    Debug("Kx132 setup complete!");
 }
 
 void LynkTree::update_battery_status()
 {
-    if (power_source(&battery_status_) == PICO_OK) {
-        power_str_ = battery_status_ ? "BATTERY" : "POWERED";
-    }
+    
+    // read the voltage at the adc0 pin
+    adc_select_input(0);
+    const float conversionFactor = 3.3f / (1 << 12);
+    float voltage = (float)adc_read() * conversionFactor;
 
-    // Get voltage
-    float voltage = 0;
-    int voltage_return = power_voltage(&voltage);
-    voltage = floorf(voltage * 100) / 100;
+    voltage = floorf(voltage * 100) / 100; //2 decimals
 
     // Display power if it's changed
-    if (old_battery_status_ != battery_status_ || old_voltage_ != voltage) {
-        if (battery_status_ && voltage_return == PICO_OK) {
-            int percent_val = (int) (((voltage - min_battery_volts_) / (max_battery_volts_ - min_battery_volts_)) * 100);
-            snprintf(percent_buf_, sizeof(percent_buf_), " (%d%%)", percent_val);
-        }
-
-        // Also get the temperature - NOT SURE HOW THIS WORKS??
-        adc_select_input(4);
-        const float conversionFactor = 3.3f / (1 << 12);
-        float adc = (float)adc_read() * conversionFactor;
-        float tempC = 27.0f - (adc - 0.706f) / 0.001721f;
-
-        old_battery_status_ = battery_status_;
-        old_voltage_ = voltage;
-    }
+    battery_percent_ = (int) (((voltage - min_battery_volts_) / (max_battery_volts_ - min_battery_volts_)) * 100);
 }   
 
 void LynkTree::loop()
@@ -209,8 +163,10 @@ void LynkTree::loop()
    
     // sleep_ms(5);
     // Debug(kxAccel_.getOperatingMode() == 0 ? "Low Power Mode" : "High Power Mode");
+    if (using_battery_) {
+        update_battery_status();
+    }
 
-    //update_battery_status();
     //Debug(("Current battery level:" + String(percent_buf_)).c_str());
 
     sleep_ms(5000);
