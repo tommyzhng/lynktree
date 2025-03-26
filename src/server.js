289f70d42aebab76b4dd4e8304284d7023d4724b
QixinL <qixin.alao@gmail.com>
2025-03-26 12:55:07 -0400

    test for local backend call

---
+++ src/server.js
@@ -0,0 +1,31 @@
+const express = require("express");
+const { spawn } = require("child_process");
+const path = require("path");
+
+const app = express();
+
+// Path to the Python script you want to run
+const pythonScriptPath = path.join(__dirname, "../python/main.py");
+
+// Spawn the Python process that runs main.py
+const pythonProcess = spawn("python", [pythonScriptPath]);
+
+// Handle the close event for the Python process
+pythonProcess.on("close", (code) => {
+  console.log(`Python process exited with code ${code}`);
+});
+
+// Serve static files for the React app
+app.use(express.static(path.join(__dirname, "build")));
+
+// This will handle all requests to your React app
+app.get("*", (req, res) => {
+  res.sendFile(path.join(__dirname, "build", "index.html"));
+});
+
+// Start the Express server (you can use any port for the API server)
+const PORT = 3001;
+app.listen(PORT, () => {
+  console.log(`Backend server is running on http://localhost:${PORT}`);
+});
+
